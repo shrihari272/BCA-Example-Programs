d6af8b4ec5248f1293eb14c8eb458092e8c774af
Shrihari <80241966+shrihari272@users.noreply.github.com>
2022-06-07 19:16:36 +0530

    Delete 2nd BCA directory

--- 2nd BCA/Programming with JAVA/sample JAVA code/inheritance/factory.java
+++
@@ -1,9 +0,0 @@
-package inheritance;//package is floder name 
-public class factory
-{
-    public int models()// it return int type value
-    {
-        System.out.println("Factory class");
-        return 99;
-    }    
-}
